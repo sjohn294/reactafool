f0df7feffdf9cbe6546b771826c6363fdbeb03b1
sjohn294 <johnson.sam26.sj@gmail.com>
2023-11-17 23:40:50 -0500

    finishing touches

--- challenge-20/src/components/pages/MyWork.jsx
+++ challenge-20/src/components/pages/MyWork.jsx
@@ -1,15 +1,21 @@
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
 import './pages.css'
+import image1 from '../../assets/3card.jpg'
+import image2 from '../../assets/5card.jpg'
+import image3 from '../../assets/6card.jpg'
+import image4 from '../../assets/11card.jpg'
+import image5 from '../../assets/12card.jpg'
+import image6 from '../../assets/18card.jpg'
 
 export default function MyWork() {
     const myProjects= [
-        {title: 'project 1', description:'pj1 describe', githubLink:'', deployedLink:'',image:''},
-        {title: 'project 2', description:'pj1 describe', githubLink:'', deployedLink:'',image:''},
-        {title: 'project 3', description:'pj1 describe', githubLink:'', deployedLink:'',image:''},
-        {title: 'project 4', description:'pj1 describe', githubLink:'', deployedLink:'',image:''},
-        {title: 'project 5', description:'pj1 describe', githubLink:'', deployedLink:'',image:''},
-        {title: 'project 6', description:'pj1 describe', githubLink:'', deployedLink:'',image:''},
+        {title: 'CreedThoughts', githubLink:'https://github.com/sjohn294/CreedThoughts', deployedLink:'https://sjohn294.github.io/CreedThoughts/',image: image1},
+        {title: 'WakeUpMrJohnson', githubLink:'https://github.com/sjohn294/Wakeupmrjohnson', deployedLink:'https://sjohn294.github.io/Wakeupmrjohnson/',image:image2},
+        {title: 'TurdParty', githubLink:'https://github.com/sjohn294/TurdParty', deployedLink:'https://sjohn294.github.io/TurdParty/',image:image3},
+        {title: 'ExpressYourself', githubLink:'https://github.com/sjohn294/Expressyourself', deployedLink:'https://safe-brushlands-38967-e17624b6bce0.herokuapp.com/',image:image4},
+        {title: 'TheSqlIsbetter', githubLink:'https://github.com/sjohn294/thesequelisbetter', deployedLink:'https://drive.google.com/file/d/1CTcU3RA8stETPtZwq2RG04H_PVhTHLk4/view',image:image5},
+        {title: 'ThankYouComeAgain', githubLink:'https://drive.google.com/file/d/18LfaoQoOOsE913IWHG-ezgzsWPXQAQQf/view', deployedLink:'https://drive.google.com/file/d/18LfaoQoOOsE913IWHG-ezgzsWPXQAQQf/view',image:image6},
     ]
     
     return (
--- challenge-20/src/components/pages/Resume.jsx
+++ challenge-20/src/components/pages/Resume.jsx
@@ -1,19 +1,45 @@
 export default function Resume() {
-    return (
+  return (
       <div>
-        <h1>Resume</h1>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed neque
-          velit, lobortis ut magna varius, blandit rhoncus sem. Morbi lacinia nisi
-          ac dui fermentum, sed luctus urna tincidunt. Etiam ut feugiat ex. Cras
-          non risus mi. Curabitur mattis rutrum ipsum, ut aliquet urna imperdiet
-          ac. Sed nec nulla aliquam, bibendum odio eget, vestibulum tortor. Cras
-          rutrum ligula in tincidunt commodo. Morbi sit amet mollis orci, in
-          tristique ex. Donec nec ornare elit. Donec blandit est sed risus feugiat
-          porttitor. Vestibulum molestie hendrerit massa non consequat. Vestibulum
-          vitae lorem tortor. In elementum ultricies tempus. Interdum et malesuada
-          fames ac ante ipsum primis in faucibus.
-        </p>
-      </div>
-    );
-  }
+          <h1>Samuel Johnson's Resume</h1>
+          
+          <h2>Professional Experience</h2>
+          <h3>Motor T Operator, United States Marine Corps</h3>
+          <p><strong>2015 - 2021</strong></p>
+          <ul>
+              <li>Operated and maintained a fleet of tactical military vehicles.</li>
+              <li>Managed logistical tasks including transport scheduling and route planning.</li>
+              <li>Ensured adherence to safety protocols, resulting in zero accidents during tenure.</li>
+              <li>Received commendation for outstanding performance and reliability.</li>
+          </ul>
+
+          <h3>Manager, International Department</h3>
+          <p><strong>2020 - 2023</strong></p>
+          <ul>
+              <li>Directed international operations, enhancing global partnerships.</li>
+              <li>Managed a diverse team, fostering an inclusive and productive work environment.</li>
+              <li>Implemented strategic plans, resulting in a 20% increase in international revenue.</li>
+              <li>Streamlined department processes, improving efficiency by 30%.</li>
+          </ul>
+
+          <h2>Education</h2>
+          <h3>Full Stack Web Development Course</h3>
+          <p><strong>University of North Carolina at Charlotte</strong> - 2023</p>
+          <ul>
+              <li>Completed intensive course covering front-end and back-end technologies.</li>
+              <li>Developed a capstone project, a web application for efficient resource management.</li>
+              <li>Gained skills in HTML, CSS, JavaScript, Node.js, and React.</li>
+          </ul>
+
+          <h2>Skills</h2>
+          <ul>
+            <li> Web Development </li> 
+          <li>Project Management</li> 
+          <li>Team Leadership</li> 
+          <li>Logistics Coordination</li> <li>Vehicle Maintenance</li> 
+          <li>Strategic Planning</li>
+          </ul>
+          </div>
+  )
+}
+
